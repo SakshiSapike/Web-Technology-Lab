2f358544e79d509e7af7770f10a8ce4f7cabb340
Sakshi Sapike <143688923+SakshiSapike@users.noreply.github.com>
2026-03-02 11:08:34 +0530

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login';
-import { RegisterComponent } from './register/register';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
-];
